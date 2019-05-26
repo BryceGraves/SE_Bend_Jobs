49c43574c3711ef9ad1e8c1ec34dfacd8646ce24
MackHatfield <therealbakoonzar@gmail.com>
2019-05-25 17:02:21 -0700

    add test for adding a new job and blank new job template

--- spec/system/add_job_spec.rb
+++ spec/system/add_job_spec.rb
@@ -3,5 +3,11 @@ require "rails_helper"
 RSpec.describe "adding a job", type: :system do
   it "allows a user to add a new job" do
     visit new_job_path
+    fill_in "Title", with: "Server"
+    fill_in "Description", with: "You will server the hungry people their food"
+    select("available", from: "availability")
+    click_on("Create Job")
+    visit jobs_path
+    expect(page).to have_content("Server")
   end
 end
