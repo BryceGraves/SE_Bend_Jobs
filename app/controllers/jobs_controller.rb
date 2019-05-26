85b2034b0b17a48c952dfd4c38bbfd6df863e951
MackHatfield <therealbakoonzar@gmail.com>
2019-05-25 17:51:08 -0700

    add blank index page and add create action to controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,19 @@ class JobsController < ApplicationController
   def new
     @job = Job.new
   end
-  # def index
-  #   @jobs = Job.all
-  # end
+
+  def index
+    @jobs = Job.all
+  end
+
+  def create
+    @job_workflow = CreatesJob.new(
+      title: params[:title],
+      description: params[:description],
+      pay: params[:pay],
+      availability: params[:availability]
+    )
+
+    @job_workflow.create
+  end
 end
--- spec/system/add_job_spec.rb
+++ spec/system/add_job_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "adding a job", type: :system do
     visit new_job_path
     fill_in "Title", with: "Server"
     fill_in "Description", with: "You will server the hungry people their food"
+    fill_in "Pay", with: 10000
     select("available", from: "availability")
     click_on("Create Job")
     visit jobs_path
