d7094b28056e8c41cf3881f15e3f59bb2270baad
Bryce Graves <brycegraves97@gmail.com>
2019-05-29 11:25:23 -0700

    CreatesJob: add tests to flesh out creating a job

--- spec/workflows/creates_job_spec.rb
+++ spec/workflows/creates_job_spec.rb
@@ -1,12 +1,27 @@
 require "rails_helper"
 
 RSpec.describe CreatesJob do
-  let(:creator) { CreatesJob.new(title: "FAKE JOB TITLE", pay: 42, description: "FAKE DESCRIPTION", available: true) }
+  let(:creator) { CreatesJob.new(title: "FAKE JOB TITLE", description: "FAKE DESCRIPTION",  pay: 42, available: true) }
 
-  describe "initialization" do
-    it "creates a job given a title" do
+  describe "When a job is created" do
+    it "It has the title specified" do
       creator.build
       expect(creator.job.title).to eq("FAKE JOB TITLE")
+    end
+
+    it "It has the description specified" do
+      creator.build
+      expect(creator.job.description).to eq("FAKE DESCRIPTION")
+    end
+
+    it "It has the pay specified" do
+      creator.build
+      expect(creator.job.pay).to eq(42)
+    end
+
+    it "It has an availability" do
+      creator.build
+      expect(creator.job.available).to eq(true)
     end
   end
 
