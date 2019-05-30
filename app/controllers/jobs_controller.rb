d392fa270fff14a39aeff6f34d58e4eb4933c286
MackHatfield <therealbakoonzar@gmail.com>
2019-05-29 23:08:28 -0700

    add business_id as hidden field to form to pass to create job action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,21 +6,21 @@ class JobsController < ApplicationController
   def new
     @business_id = params[:business_id] 
     @job = Job.new
-    byebug
   end
 
   def create
+    byebug
     @job_workflow = CreatesJob.new(
       title: params[:job][:title],
       description: params[:job][:description],
       pay: params[:job][:pay],
       available: params[:available],
-      business_id: @business_id
+      business_id: params[:business_id]
     )
 
     @job_workflow.create
 
-    redirect_to jobs_path
+    redirect_to controller: 'businesses', action: 'show', id: @business_id
   end
   
 end
