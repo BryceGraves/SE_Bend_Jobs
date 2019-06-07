11b0d094f53a287de38265dee8ea5ec57caf275d
Bryce Graves <brycegraves97@gmail.com>
2019-06-07 15:39:23 -0700

    BusinessSpec: testing the business hiring functionality
    The tests cover if a business has one job that is unavailable, if it has many jobs but has one available, and if it has many with none available

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -3,9 +3,11 @@ require "rails_helper"
 RSpec.describe Business do
   let(:valid_business_no_jobs) { FactoryBot.build_stubbed(:business) }
   let(:valid_business_one_job) { FactoryBot.build_stubbed(:business, :with_one_job) }
+  let(:valid_business_one_unavailable_job) { FactoryBot.build_stubbed(:business, :with_one_unavailable_job) }
   let(:valid_business_many_jobs) { FactoryBot.build_stubbed(:business, :with_many_jobs) }
+  let(:valid_business_no_available_jobs) { FactoryBot.build_stubbed(:business, :with_no_available_jobs) }
 
-  describe "A business is valid if" do 
+  describe "A business is valid if" do
     it "It has a name" do
       expect(valid_business_no_jobs.name).to eq("FAKE BUSINESS")
 
@@ -31,6 +33,21 @@ RSpec.describe Business do
 
       expect(valid_business_no_jobs).to be_valid
     end
+  end
+
+  describe "Business functionality" do
+    context "A business can check if has a hiring job available" do
+      it "If it has one job" do
+        expect(valid_business_one_job.hiring?).to eq(true)
+        expect(valid_business_one_unavailable_job.hiring?).to eq(false)
+      end
+
+      it "If it has many jobs with varying availability" do
+        expect(valid_business_many_jobs.hiring?).to eq(true)
+        expect(valid_business_no_available_jobs.hiring?).to eq(false)
+      end
+    end
+
   end
 
 end
