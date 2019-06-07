8df3fd3fa4ff63e419015c4eafd34e9431f78167
Bryce Graves <brycegraves97@gmail.com>
2019-06-07 15:37:50 -0700

    BusinessFactory: add factories for businesses with unavailable jobs and aren't hiring

--- spec/factories/business.rb
+++ spec/factories/business.rb
@@ -11,17 +11,38 @@ FactoryBot.define do
         }
     end
 
+    trait :with_one_unavailable_job do
+      jobs {
+        [
+          Job.new(title: "FAKE UNAVAILABLE JOB", pay: 92000, description: "FAKE DESCRIPTION", available: false)
+        ]
+      }
+  end
+
     trait :with_many_jobs do
       jobs {
           [
             Job.new(title: "FAKE JOB ONE", pay: 1, description: "FAKE DESCRIPTION", available: true),
-            Job.new(title: "FAKE JOB TWO", pay: 2, description: "FAKE DESCRIPTION", available: true),
+            Job.new(title: "FAKE JOB TWO", pay: 2, description: "FAKE DESCRIPTION", available: false),
             Job.new(title: "FAKE JOB THREE", pay: 3, description: "FAKE DESCRIPTION", available: true),
-            Job.new(title: "FAKE JOB FOUR", pay: 4, description: "FAKE DESCRIPTION", available: true),
+            Job.new(title: "FAKE JOB FOUR", pay: 4, description: "FAKE DESCRIPTION", available: false),
             Job.new(title: "FAKE JOB FIVE", pay: 5, description: "FAKE DESCRIPTION", available: true)
-          ] 
+          ]
         }
     end
+
+    trait :with_no_available_jobs do
+      jobs {
+          [
+            Job.new(title: "FAKE JOB ONE", pay: 1, description: "FAKE DESCRIPTION", available: false),
+            Job.new(title: "FAKE JOB TWO", pay: 2, description: "FAKE DESCRIPTION", available: false),
+            Job.new(title: "FAKE JOB THREE", pay: 3, description: "FAKE DESCRIPTION", available: false),
+            Job.new(title: "FAKE JOB FOUR", pay: 4, description: "FAKE DESCRIPTION", available: false),
+            Job.new(title: "FAKE JOB FIVE", pay: 5, description: "FAKE DESCRIPTION", available: false)
+          ]
+        }
+    end
+
   end
 
 end
