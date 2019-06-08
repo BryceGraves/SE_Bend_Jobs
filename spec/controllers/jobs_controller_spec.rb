9d215b6438e98e02dd3d9931c62205f355016694
Bryce Graves <brycegraves97@gmail.com>
2019-06-07 20:50:30 -0700

    JobsControllerSpec: add get test for index

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe JobsController, type: :controller do
-
+  describe "Jobs index page" do
+    it "Returns status code 200" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+  
 end
