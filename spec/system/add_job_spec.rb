05b6de3ed41f5194910783870a0e8c4cf1d0a7e4
Mackenzie Hatfield <mackhatfield@10-116-52-75.wireless.oregonstate.edu>
2019-06-09 17:57:22 -0700

    add spec for navigating pages

--- spec/system/add_job_spec.rb
+++ spec/system/add_job_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "adding a job to a business", type: :system do
+RSpec.describe "Adding a job to a business", type: :system do
   it "can add add a new job to a business" do
     visit new_business_path
     fill_in "Name", with: "FAKE BUSINESS"
