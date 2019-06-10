de81be6d2e158abdd7371a4601fc2a108cc1632e
Mackenzie Hatfield <mackhatfield@MacBook-Pro.local>
2019-06-10 02:12:23 -0700

    update add business system spec to handle file uploading

--- spec/system/add_business_spec.rb
+++ spec/system/add_business_spec.rb
@@ -10,6 +10,23 @@ RSpec.describe "Add business", type: :system do
     expect(page).to have_content("Food Place")
   end
 
+  it "User can upload an image to be used as a logo" do
+    visit new_business_path
+    fill_in "Name", with: "Applebees"
+    fill_in "Business type", with: "Casual Dining"
+    attach_file("business_logo", Rails.root + "public/nick.jpg")
+    click_on("Create Business")
+    expect(page).to have_css("#custom-logo")
+  end
+
+  it "The logo will be a default image if the user doesn't upload one" do
+    visit new_business_path
+    fill_in "Name", with: "LOGOLESS BUSINESS"
+    fill_in "Business type", with: "we're not sure"
+    click_on("Create Business")
+    expect(page).to have_css("#default-logo")
+  end
+
   it "User is redirected back to page if create fails do to blank or duplicate name" do
     visit new_business_path
     fill_in "Name", with: ""
