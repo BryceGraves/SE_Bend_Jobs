3ef7784247d7fa5e3e89b62dddf4f0c4093f2d4e
Bryce Graves <brycegraves97@gmail.com>
2019-06-10 10:46:37 -0700

    RemoveBusinessSpec: Add test that shows that deleting a business deletes the jobs associated

--- spec/system/business/remove_business_spec.rb
+++ spec/system/business/remove_business_spec.rb
@@ -10,4 +10,42 @@ RSpec.describe "Remove a business", type: :system do
     expect(page).not_to have_content("FAKE BUSINESS")
     expect(page).not_to have_content("FAKE TYPE")
   end
+
+  it "When a user removes a business it removes all jobs associated with it." do
+    visit new_business_path
+    fill_in "Name", with: "FAKE BUSINESS"
+    fill_in "Business type", with: "FAKE TYPE"
+    click_on("Create Business")
+    click_link('add-job-FAKE BUSINESS')
+    expect(page).to have_content("Add a Job")
+    fill_in "Title", with: "FAKE TITLE ONE"
+    fill_in "Description", with: "FAKE DESCRIPTION"
+    fill_in "Pay", with: 1
+    select("available", :from => "availability")
+    click_on("Create Job")
+    click_link('add-job-FAKE BUSINESS')
+    expect(page).to have_content("Add a Job")
+    fill_in "Title", with: "FAKE TITLE TWO"
+    fill_in "Description", with: "FAKE DESCRIPTION"
+    fill_in "Pay", with: 2
+    select("available", :from => "availability")
+    click_on("Create Job")
+    click_link('add-job-FAKE BUSINESS')
+    expect(page).to have_content("Add a Job")
+    fill_in "Title", with: "FAKE TITLE THREE"
+    fill_in "Description", with: "FAKE DESCRIPTION"
+    fill_in "Pay", with: 3
+    select("available", :from => "availability")
+    click_on("Create Job")
+    visit jobs_path
+    expect(page).to have_content("FAKE TITLE ONE")
+    expect(page).to have_content("FAKE TITLE TWO")
+    expect(page).to have_content("FAKE TITLE THREE")
+    visit businesses_path
+    click_link("delete-business-FAKE BUSINESS")
+    visit jobs_path
+    expect(page).not_to have_content("FAKE TITLE ONE")
+    expect(page).not_to have_content("FAKE TITLE TWO")
+    expect(page).not_to have_content("FAKE TITLE THREE")
+  end
 end
