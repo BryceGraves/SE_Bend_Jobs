36209d65eb68fa46d54e820d7994536469df2459
Bryce Graves <brycegraves97@gmail.com>
2019-06-09 17:10:44 -0700

    Routing: Jobs now routes through business unless pinging all jobs page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :jobs
-  resources :businesses
+  resources :businesses do
+    resources :jobs, except: [ :index ]
+  end
+
+  resources :jobs, only: [ :index ]
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
